337ae5be1de8e94da6e083a3a976268ee70a61f8
Knobloch Moshe <moskno1@gmail.com>
2025-08-14 13:47:52 +0300

    update the file

---
+++ data-loader/scripts/insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO table_for_project (first_name,last_name) VALUES ('yonatan', 'ron');
+INSERT INTO table_for_project (first_name,last_name) VALUES ('david', 'teichman');
+INSERT INTO table_for_project (first_name,last_name) VALUES ('tzipora', 'teichman');
